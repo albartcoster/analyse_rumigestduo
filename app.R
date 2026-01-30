75b103051fe321fe8d0edce5b7e97a89e2c0c3a8
Albart Coster <albart@dairyconsult.nl>
2026-01-30 22:17:07 +0100

    minder iteraties

--- app.R
+++ app.R
@@ -21,7 +21,7 @@ ui = fluidPage(
      $(document).on('shiny:connected', function(event) {
      socket_timeout_interval = setInterval(function(){
      Shiny.onInputChange('count', n++)
-     }, 15000)
+     }, 15)
      });
      $(document).on('shiny:disconnected', function(event) {
      clearInterval(socket_timeout_interval)
